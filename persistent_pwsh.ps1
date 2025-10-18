6abc2dc8bf0a2edc7199c086dc36acca486927a5
David Peled <davidp.info1@gmail.com>
2025-10-18 13:29:38 +0300

    cleanups

--- persistent_pwsh.ps1
+++
@@ -1,7 +0,0 @@
-while ($true) {
-    Write-Host "Starting PowerShell..."
-    pwsh   # or powershell.exe depending on your version
-    Write-Host "PowerShell exited. Restarting in 2 seconds..."
-    Start-Sleep -Seconds 2
-}
-
